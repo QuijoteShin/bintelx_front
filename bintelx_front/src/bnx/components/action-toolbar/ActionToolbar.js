eafa58e02840ca5ab9fd810fb62d84a4fb6c59eb
QuijoteShin <quijote.shin@gmail.com>
2025-06-12 07:34:45 -0400

    + ActionToolbar

---
+++ bintelx_front/src/bnx/components/action-toolbar/ActionToolbar.js
@@ -0,0 +1,47 @@
+// src/bnx/components/action-toolbar/ActionToolbar.js
+
+class ActionToolbar {
+    /**
+     * @param {HTMLElement} element The DOM element for the toolbar.
+     */
+    constructor(element) {
+        if (!element) {
+            throw new Error('A valid DOM element is required for ActionToolbar.');
+        }
+        this.element = element;
+    }
+
+    /**
+     * Shows the toolbar by removing the state class.
+     */
+    show() {
+        this.element.classList.remove('is-hidden');
+    }
+
+    /**
+     * Hides the toolbar by adding a state class.
+     */
+    hide() {
+        this.element.classList.add('is-hidden');
+    }
+
+    /**
+     * Toggles the visibility of the toolbar.
+     */
+    toggle() {
+        this.element.classList.toggle('is-hidden');
+    }
+
+    /**
+     * Checks if the toolbar is currently visible.
+     * @returns {boolean}
+     */
+    isVisible() {
+        // Visibility is determined by the absence of the 'is-hidden' class.
+        return !this.element.classList.contains('is-hidden');
+    }
+
+    // Future enhancements for scroll handling would live here.
+}
+
+export default ActionToolbar;
