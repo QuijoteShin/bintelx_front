703bc73aa9953e6e05167079be836e7455173ab1
QuijoteShin <quijote.shin@gmail.com>
2025-06-14 02:19:41 -0400

    Update config.js
    CONFIG ENDPOINTS + AUTH

--- bintelx_front/src/config.js
+++ bintelx_front/src/config.js
@@ -5,7 +5,11 @@ export const config = {
     defaultRoute: '/_dashboard/index',
     appContainer: { loading: '<h2>Loading...</h2>' },
     api: {
-        baseUrl: '/api/mock',
+        baseUrl: '/api/v1',
         timeout: 15000
     },
+    AUTH_TOKEN_NAME: 'bnxt',
+    AUTH_TOKEN_VALIDATE_ENDPOINT: '/_auth/validate',
+    DEV_TOOLS_OPENED_ENDPOINT: '/_security/report',
+    ENDPOINT_BASE_URL: '',
 };
